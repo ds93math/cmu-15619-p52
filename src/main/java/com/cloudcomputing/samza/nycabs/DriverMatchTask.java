48649da9dd7beb2f0ec46316b2d5c5d56498f2f6
ds93math <su.david2016@gmail.com>
2023-11-19 17:16:57 -0800

    push push

--- src/main/java/com/cloudcomputing/samza/nycabs/DriverMatchTask.java
+++ src/main/java/com/cloudcomputing/samza/nycabs/DriverMatchTask.java
@@ -13,6 +13,10 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 /**
  * Consumes the stream of driver location updates and rider cab requests.
@@ -26,6 +30,7 @@ public class DriverMatchTask implements StreamTask, InitableTask {
     */
     private KeyValueStore<String, String> driverLocationStore;
     private KeyValueStore<String, String> clientRequestStore;
+    private ObjectMapper objectMapper;
     private double MAX_MONEY = 100.0;
 
     private JSONUtil jsonUtil;
@@ -37,6 +42,7 @@ public class DriverMatchTask implements StreamTask, InitableTask {
         this.clientRequestStore = (KeyValueStore<String, String>) context.getTaskContext().getStore("client-request-store");
         this.driverLocationStore = (KeyValueStore<String, String>) context.getTaskContext().getStore("driver-loc");
         this.jsonUtil = new JSONUtil();
+        this.objectMapper = new ObjectMapper()
     }
 
     @Override
@@ -51,18 +57,80 @@ public class DriverMatchTask implements StreamTask, InitableTask {
         Map<String, Object> message = (Map<String, Object>) envelope.getMessage();
         String messageType = (String) message.get("type");
 
-        if ("ENTERING_BLOCK".equals(messageType)) {
-            // Handle driver entering block
-            // Update driver location and state in the driverLocationStore
-        } else if ("LEAVING_BLOCK".equals(messageType)) {
-            // Handle driver leaving block
-            // Remove or update driver state in the driverLocationStore
+         if ("ENTERING_BLOCK".equals(messageType) || "LEAVING_BLOCK".equals(messageType)) {
+            // Handle driver entering or leaving block
+            handleDriverLocationUpdates(message, messageType);
         } else if ("RIDE_REQUEST".equals(messageType)) {
             // Handle ride request
-            // Find the best matching driver based on the provided match score formula
-            // Use helper methods to calculate each score component
+            handleRideRequest(message, collector);
+        }
+    }
 
-            // Example of calculating match score (you will need actual driver details and client preferences)
+    private void handleDriverLocationUpdates(Map<String, Object> message, String messageType) {
+        // Logic for handling driver location updates <<<<<<<!!!!!!!!!!!!!!
+    }
+
+    private void handleRideRequest(Map<String, Object> message, MessageCollector collector) {
+        // Parse the client request message
+        Integer clientId = (Integer) message.get("clientId");
+        Double clientLatitude = (Double) message.get("latitude");
+        Double clientLongitude = (Double) message.get("longitude");
+        String genderPreference = (String) message.get("gender_preference");
+        Integer blockId = (Integer) message.get("blockId");
+
+        // Retrieve all available drivers in the same block
+        // Assuming `getAllDriversInBlock` is a method to retrieve drivers
+        List<Driver> driversInBlock = getAllDriversInBlock(blockId);
+        Driver bestMatch = null;
+        double highestMatchScore = -1;
+
+        for (Driver driver : driversInBlock) {
+            // Get driver details from the store or the message
+            double distanceScore = calculateDistanceScore(driver, clientLatitude, clientLongitude);
+            double genderScore = calculateGenderScore(driver, genderPreference);
+            double ratingScore = calculateRatingScore(driver.getRating());
+            double salaryScore = calculateSalaryScore(driver.getSalary());
+
+            double matchScore = distanceScore * 0.4 + genderScore * 0.1 + ratingScore * 0.3 + salaryScore * 0.2;
+
+            if (matchScore > highestMatchScore) {
+                highestMatchScore = matchScore;
+                bestMatch = driver;
+            }
+        }
+
+        if (bestMatch != null) {
+            // Construct the match JSON object to send to the match stream
+            Map<String, Integer> match = new HashMap<>();
+            match.put("clientId", clientId);
+            match.put("driverId", bestMatch.getDriverId());
+            String matchJson = toJson(match);
+            
+            // Send the match to the match-stream
+            collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", "match-stream"), matchJson));
+        }
+    }
+
+    // Helper methods for calculating scores  <<<<<<<!!!!!!!!!!!!!!<<<<<<<!!!!!!!!!!!!!!
+    private double calculateDistanceScore(Driver driver, double clientLatitude, double clientLongitude) {
+        // Calculate the distance score based on driver and client coordinates
+    }
+
+    //<<<<<<<!!!!!!!!!!!!!!<<<<<<<!!!!!!!!!!!!!!<<<<<<<!!!!!!!!!!!!!!
+    private double calculateGenderScore(Driver driver, String genderPreference) {
+        // Calculate the gender score based on driver gender and client preference
+    }
+
+    private double calculateRatingScore(double driverRating) {
+        return driverRating / 5.0;
+    }
+
+    private double calculateSalaryScore(double driverSalary) {
+        return 1 - (driverSalary / 100.0);
+    }
+    
+    /*
+    // Example of calculating match score (you will need actual driver details and client preferences)
             double distanceScore = calculateDistanceScore(driverLatitude, driverLongitude, clientLatitude, clientLongitude);
             double genderScore = calculateGenderScore(driverGender, clientGenderPreference);
             double ratingScore = calculateRatingScore(driverRating);
@@ -78,7 +146,7 @@ public class DriverMatchTask implements StreamTask, InitableTask {
             collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", "match-stream"), match));
         }
 
-        /*String incomingStream = envelope.getSystemStreamPartition().getStream();
+        //String incomingStream = envelope.getSystemStreamPartition().getStream();
 
         if (incomingStream.equals(DriverMatchConfig.DRIVER_LOC_STREAM.getStream())) {
 	    // Handle Driver Location messages
@@ -98,8 +166,23 @@ public class DriverMatchTask implements StreamTask, InitableTask {
             }
         } else {
             throw new IllegalStateException("Unexpected input stream: " + envelope.getSystemStreamPartition());
-        }*/
+        }
+    } 
+    */
+    private String toJson(Map<String, Integer> data) {
+        try {
+            return objectMapper.writeValueAsString(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Error serializing data to JSON", e);
+        }
     }
+
+    //<<<<<<<!!!!!!!!!!!!!!<<<<<<<!!!!!!!!!!!!!!<<<<<<<!!!!!!!!!!!!!!
+    // Assume a Driver class is defined elsewhere with appropriate fields and methods
+    private List<Driver> getAllDriversInBlock(Integer blockId) {
+        // Implement logic to retrieve all drivers in the block from the driverLocationStore
+    }
+
 
     private void sendMatchToOutputStream(MessageCollector collector, Integer clientId, Integer driverId) {
         Map<String, Object> matchMap = new HashMap<>();
