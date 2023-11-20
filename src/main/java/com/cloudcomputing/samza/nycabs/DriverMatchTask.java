9c1e58702f95cc1dd3b8136e35bb90c03e60f0d8
ds93math <su.david2016@gmail.com>
2023-11-19 18:59:21 -0800

    sd

--- src/main/java/com/cloudcomputing/samza/nycabs/DriverMatchTask.java
+++ src/main/java/com/cloudcomputing/samza/nycabs/DriverMatchTask.java
@@ -19,6 +19,7 @@ import java.util.stream.Collectors;
 import java.util.Map.Entry;
 import java.io.IOException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
 
